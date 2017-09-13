9b5b5a9437203e3014f631461c94bd747168e31e
CMYanko <cmyanko@gmail.com>
2017-09-12 21:44:42 -0400

    fixed directory copy and detached to startup from the terminal

--- demo-setup.sh
+++ demo-setup.sh
@@ -2,7 +2,7 @@
 
 # Creates directories to be mounted to containers as volumes
 mkdir ~/iq-data ~/nexus-data
-cp ./nexus-ssl ~/nexus-ssl
+cp -R ./nexus-ssl ~/nexus-ssl
 
 # Stands up test environment
-docker-compose up
+docker-compose up -d
