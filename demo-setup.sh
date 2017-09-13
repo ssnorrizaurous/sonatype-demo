b62f2e12d032f13757a58df93c5bbf4abff123d9
CMYanko <cmyanko@gmail.com>
2017-09-12 21:34:52 -0400

    adding nexus-ssl folder and updating script to put SSL cert in place. Plus updated YML file to have ports for Docker

--- demo-setup.sh
+++ demo-setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Creates directories to be mounted to containers as volumes
-mkdir ~/iq-data ~/nexus-data ~/nexus-ssl
+mkdir ~/iq-data ~/nexus-data
+cp ./nexus-ssl ~/nexus-ssl
 
 # Stands up test environment
 docker-compose up
